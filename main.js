d5d9d722963274e4eba9e6fd7bdb39a490fe91d8
theseventhgod <tobyadeyy@gmail.com>
2023-07-08 09:04:05 +0100

    new update

---
+++ main.js
@@ -0,0 +1,125 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  let currentPlayer = "X";
+
+  let gameActive = false;
+  let playAgainstComputer = true;
+
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columns
+    [0, 4, 8],
+    [2, 4, 6], // Diagonals
+  ];
+
+  const boxes = document.querySelectorAll(".game-area span");
+  const playAgainstComputerBtn = document.getElementById("cpu-mode");
+  const playAgainstFriendBtn = document.getElementById("friend-mode");
+  const restartBtn = document.getElementById("Restart");
+  const resultBox = document.querySelector(".result-box");
+  const closeBtn = document.querySelector(".close");
+  const msgElement = document.querySelector(".notifier h1");
+
+  function handlePlayModeSelection(event) {
+    playAgainstComputer = event.target.id === "cpu-mode";
+    document.querySelector(".play-mode").style.display = "none";
+    document.querySelector(".select-player").style.display = "flex";
+  }
+
+  function handlePlayerSelection(event) {
+    currentPlayer = event.target.id === "Xplayer" ? "X" : "O";
+    document.querySelector(".select-player").style.display = "none";
+    document.querySelector(".game-room").style.display = "flex";
+    startGame();
+  }
+
+  function startGame() {
+    gameActive = true;
+    resultBox.style.display = "none";
+    boxes.forEach((box) => {
+      box.textContent = "";
+      box.addEventListener("click", handleBoxClick, { once: true });
+    });
+
+    if (currentPlayer === "O" && playAgainstComputer) {
+      makeComputerMove();
+    }
+  }
+
+  function handleBoxClick(event) {
+    const boxIndex = event.target.id;
+    if (board[boxIndex] === " " && gameActive) {
+      board[boxIndex] = currentPlayer;
+      event.target.textContent = currentPlayer;
+
+      if (checkWin(currentPlayer)) {
+        endGame(`Player ${currentPlayer} wins!`);
+      } else if (isBoardFull()) {
+        endGame("It's a Draw!");
+      } else {
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        if (playAgainstComputer && currentPlayer === "O") {
+          makeComputerMove();
+        }
+      }
+    }
+  }
+
+  function makeComputerMove() {
+    const availableMoves = [];
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === " ") {
+        availableMoves.push(i);
+      }
+    }
+    const randomIndex = Math.floor(Math.random() * availableMoves.length);
+    const position = availableMoves[randomIndex];
+    board[position] = currentPlayer;
+    boxes[position].textContent = currentPlayer;
+
+    if (checkWin(currentPlayer)) {
+      endGame(`Player ${currentPlayer} wins!`);
+    } else if (isBoardFull()) {
+      endGame("It's a Draw!");
+    } else {
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+  }
+
+  function checkWin(player) {
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (board[a] === player && board[b] === player && board[c] === player) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function isBoardFull() {
+    return !board.includes(" ");
+  }
+
+  function endGame(message) {
+    gameActive = false;
+    resultBox.style.display = "flex";
+    msgElement.textContent = message;
+    boxes.forEach((box) => {
+      box.removeEventListener("click", handleBoxClick);
+    });
+  }
+
+  playAgainstComputerBtn.addEventListener("click", handlePlayModeSelection);
+  playAgainstFriendBtn.addEventListener("click", handlePlayModeSelection);
+  restartBtn.addEventListener("click", startGame);
+  closeBtn.addEventListener("click", () => {
+    resultBox.style.display = "none";
+  });
+  document.querySelectorAll(".playericon input").forEach((input) => {
+    input.addEventListener("click", handlePlayerSelection);
+  });
+});
